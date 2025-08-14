fb8ddb749c904574ca152f016bfb0f1373a85a32
Alexander Williamson <awilliamson@acidc.local>
2025-08-14 14:23:15 -0700

    Change to work with Network Drives

--- app.py
+++ app.py
@@ -11,23 +11,16 @@ nas_user = os.getenv("NAS_U")
 nas_passw = os.getenv("NAS_P")
 nas_ip = os.getenv("NAS_IP")
 
-eweb_path = r"\Program Files (x86)\Delta Controls\enteliWEB\website\support\eweb_backup"
 eweb_base = r'"\Program Files (x86)\Delta Controls\enteliWEB\website\support\eweb_backup"'
-desktop_base = r"C:\Users\awilliamson\Desktop"
-
-nas_client = SSHClient()
-nas_client.set_missing_host_key_policy(AutoAddPolicy)
-nas_client.connect(nas_ip,username=nas_user, password=nas_passw)
+eweb_path = r"\Program Files (x86)\Delta Controls\enteliWEB\website\support\eweb_backup"
+nas_base = r"Z:\Server Backups"
 
 
-
+backup_array = []
 with open(".\\server.json") as file:
     servers = json.load(file)
 
     for server in servers:
-        print(server)
-
-
         server_client = SSHClient()
         server_client.set_missing_host_key_policy(AutoAddPolicy)
 
@@ -42,27 +35,32 @@ with open(".\\server.json") as file:
         lines = backup_files.split("     ")
 
         final_file_name = None
-        # date_string = datetime.now().strftime("%Y%m%d")
+        date_string = datetime.now().strftime("%Y%m%d")
 
         with server_client.open_sftp() as sftp:
             for index, line in enumerate(lines):
                 if "Backup" in line:
-                    filename_arr = line.split("\r")[0].split(" ")
+                    fileline_arr = line.split("\r\n")
+
+                    file_date = datetime.strptime(fileline_arr[-1].split(" ")[0], "%m/%d/%Y")
+
+                    today = datetime.now()
+
+                    delta = today - file_date
+
+                    filename_arr = fileline_arr[0].split(" ")
 
                     if len(filename_arr) > 2:
                         final_file_name= filename_arr[1]+" "+filename_arr[2]+" "+filename_arr[3]
                     else:
                         final_file_name= filename_arr[1]
-                    
-                    sftp.get(eweb_path+"\\"+final_file_name,desktop_base+"\\"+final_file_name)
+
+                    sftp.get(eweb_path+"\\"+final_file_name,nas_base+"\\"+server["name"]+"\\"+final_file_name)
 
             
             sftp.close()
         
         server_client.close()
-
-
-nas_client.connect()
         
 
 
